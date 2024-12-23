67713036118c812e2dcb987e9f19a6ed8f89c72c
vrejaunier <valentin.rejaunier@hetic.net>
2024-12-23 22:19:53 +0100

    Adds update status to task.updated topic

--- services/tasks-service/tasks/task_runner.py
+++ services/tasks-service/tasks/task_runner.py
@@ -1,7 +1,8 @@
 import asyncio
 import json
+import os
 import random
-from datetime import datetime
+from tasks.kafka import Producer
 from typing import Optional
 
 from confluent_kafka import Message  # type: ignore # noqa: PGH003
@@ -17,17 +18,17 @@ class TaskCreatedMessage(BaseModel):
 
 async def handle_task_created(msg: Message) -> None:
     try:
+        producer = Producer(uri=os.getenv("KAFKA_URI"))
         logger.info("Processing Task created message")
         deserialized: dict = json.loads(msg.value().decode("utf-8"))
         data = TaskCreatedMessage.model_validate(deserialized)
         logger.info(f"id: {data.id}")
         await asyncio.sleep(10)
         data.status = "RUNNING"
+        producer.send_json("task.updated", {"id": data.id, "status": data.status})
         await asyncio.sleep(20)
         data.status = random.choice(['SUCCESS', 'FAILED'])
+        producer.send_json("task.updated", {"id": data.id, "status": data.status})
     except ValidationError as e:
         logger.error(e, exc_info=True)
         return
-
-def random_status():
-    return random.choice(['created', 'in_progress', 'done', 'failed'])
